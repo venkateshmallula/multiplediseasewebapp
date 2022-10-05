f6363e2aa91de56a3c4705c8abdccfc56f18b336
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-06 02:21:31 +0530

    Update Muliplediseaseprediction.py

--- Muliplediseaseprediction.py
+++ Muliplediseaseprediction.py
@@ -105,19 +105,19 @@ if (selected == 'Heart Disease Prediction'):
     import base64
     def add_bg_from_local(image_file):
         with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        st.markdown(
-        f"""
-        <style>
-        .stApp {{
-        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-        background-size: cover;
-        font-color: black;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-        )
+            encoded_string = base64.b64encode(image_file.read())
+            st.markdown(
+            f"""
+            <style>
+            .stApp {{
+            background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+            background-size: cover;
+            font-color: black;
+            }}
+            </style>
+            """,
+            unsafe_allow_html=True
+            )
     add_bg_from_local('h1.jpg')
     
     # page title
