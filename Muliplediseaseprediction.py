0dce3c49ada319e89dafb0b39448571f293246eb
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-06 04:15:29 +0530

    Update Muliplediseaseprediction.py

--- Muliplediseaseprediction.py
+++ Muliplediseaseprediction.py
@@ -56,7 +56,7 @@ if (selected == 'Diabetes Prediction'):
             """,
             unsafe_allow_html=True
             )
-    add_bg_from_local('d.jpg')
+    add_bg_from_local('1.jpg')
     
     # page title
     st.title('Diabetes Prediction using ML')
