18a34ed344e062a717b39f85fc0ca698f5753aef
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2022-10-06 01:40:32 +0530

    Update Muliplediseaseprediction.py

--- Muliplediseaseprediction.py
+++ Muliplediseaseprediction.py
@@ -54,23 +54,23 @@ with st.sidebar:
 if (selected == 'Diabetes Prediction'):
     
     #for adding Image
-import base64
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-        background-size: cover;
-        text-color: black;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('h1.jpg')
+    import base64
+    def add_bg_from_local(image_file):
+        with open(image_file, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+            st.markdown(
+            f"""
+            <style>
+            .stApp {{
+            background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+            background-size: cover;
+            text-color: black;
+            }}
+            </style>
+            """,
+            unsafe_allow_html=True
+            )
+    add_bg_from_local('h1.jpg')
     
     # page title
     st.title('Diabetes Prediction using ML')
