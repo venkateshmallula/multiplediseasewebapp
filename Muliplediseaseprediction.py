4f799ea7446123afe887498fc962441c5d866a1e
Venkatesh Mallula <110535555+venkateshmallula@users.noreply.github.com>
2023-06-30 00:47:09 +0530

    Update Muliplediseaseprediction.py

--- Muliplediseaseprediction.py
+++ Muliplediseaseprediction.py
@@ -57,7 +57,7 @@ if (selected == 'Diabetes Prediction'):
   #for adding background-image  
   import base64
   def add_bg_from_local(image_file):
-        with open(image_file, "rb") as image_file:
+      with open(image_file, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
             st.markdown(
             f"""
@@ -71,7 +71,7 @@ if (selected == 'Diabetes Prediction'):
             """,
             unsafe_allow_html=True
             )
-    add_bg_from_local('diabetes.jpg')
+ add_bg_from_local('diabetes.jpg')
 
     # Set the page title
     html_temp = """
